be56ba6a707bc1259fc3970b7b5bb0825df44295
Yuya Yoshida <yoshidayuya@gmail.com>
2012-01-10 06:47:07 +0900

    fix file path

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get -y upgrade
 apt-get install -y curl
-curl https://raw.github.com/gist/1176137/cdcd77e1ede950de1d598b701ecd92fa8de18425/setup_in_startup_script.sh | sh | tee /root/_setup.log
+curl https://raw.github.com/yuyalush/startup_for_ror/master/setup_in_statup_script.sh | sh | tee /root/_setup.log
 /bin/sed -i.orig -e "s/apt/#apt/g" /etc/rc.local
 /bin/sed -i.orig -e "s/curl/#curl/g" /etc/rc.local
 /bin/sed -i.orig -e "s/\/bin\/sed/#\/bin\/sed/g" /etc/rc.local

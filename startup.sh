2154fe25ff6ccd5fdb3888f7f8dd96e33dbb179c
Yuya Yoshida <yoshidayuya@gmail.com>
2012-01-10 07:09:38 +0900

    fix
    use sudo

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 /bin/cat <<EOF > /etc/rc.local
-apt-get update
-apt-get -y upgrade
-apt-get install -y curl
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y curl
 curl https://raw.github.com/yuyalush/startup_for_ror/master/setup_in_statup_script.sh | sh | tee /root/_setup.log
 /bin/sed -i.orig -e "s/apt/#apt/g" /etc/rc.local
 /bin/sed -i.orig -e "s/curl/#curl/g" /etc/rc.local
